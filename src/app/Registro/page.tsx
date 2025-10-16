d7aacd8ff1c1e15806a45b83e20945ba260f8303
Benjamin Muñoz <benja.munoz.ramirez@gmail.com>
2025-10-16 15:40:10 -0300

    added login and register forms and updated respective pages

--- src/app/Registro/page.tsx
+++ src/app/Registro/page.tsx
@@ -1,14 +1,13 @@
 import { HeaderNav } from "@/components/ui/Header";
-import LoginForm from "@/components/ui/Login";
+import Register from "@/components/ui/Register/RegisterForm";
 export default function ExpediaHeader() {
 
   return (
     <>
       <HeaderNav />
       <div className="max-w-7xl mx-auto px-4 py-8">
-        <LoginForm />
+        <Register />
       </div>
-
     </>
   );
 }
