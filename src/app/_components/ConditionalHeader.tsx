924cee295636b9227295d171b445752e7c46f357
Khav Kivar <sebastian.calderon@sansano.usm.cl>
2025-11-25 13:00:24 -0300

    feat: dont show header when is on fullscreen

--- src/app/_components/ConditionalHeader.tsx
+++ src/app/_components/ConditionalHeader.tsx
@@ -11,7 +11,7 @@ export default function ConditionalHeader(conditionalHeaderProps: ConditionalHea
   const pathname = usePathname();
 
   // Pages that should not show the header
-  const noHeaderPages = ['/map', '/booking'];
+  const noHeaderPages = ['/map', '/booking', '/fullscreen'];
 
   if (noHeaderPages.includes(pathname)) {
     return <></>;
